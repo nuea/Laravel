35c29b101cd8069de2426b30a6e3f569e00903b8
Nuea <nueha.pimkanok@gmail.com>
2018-04-23 16:00:22 +0700

    Access Control

--- myapp/app/Http/Controllers/PostsController.php
+++ myapp/app/Http/Controllers/PostsController.php
@@ -9,6 +9,15 @@ use DB;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['index','show']]);
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -78,8 +87,11 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
         $posts = Post::find($id);
+        //Check for correct user
+        if(auth()->user()->id !== $posts->user_id){
+            return redirect('/posts')->with('error','Unauthorized Page');
+        }
         return view('posts.edit')->with('posts',$posts);
     }
 
@@ -117,8 +129,11 @@ class PostsController extends Controller
     {
         //
         $post = Post::find($id);
+        //Check for correct user
+        if(auth()->user()->id !== $post->user_id){
+            return redirect('/posts')->with('error','Unauthorized Page');
+        }
         $post->delete();
-
         return redirect('/posts')->with('success','Post Remove');
     }
 }
--- myapp/resources/views/posts/show.blade.php
+++ myapp/resources/views/posts/show.blade.php
@@ -8,13 +8,16 @@
             <p class="card-text">{{$posts->body}}</p>
             <p class="card-text"><small>Written on {{$posts->created_at}} by {{$posts->user->name}}</small></p>
             <a href="/posts" class="btn btn-secondary">Go Back</a>
-
-            {{ Form::open(['action' => ['PostsController@destroy', $posts->id],'method'=>'POST','class'=>'float-right']) }}
-                {{Form::hidden('_method','DELETE')}}
-                {{ Form::submit('Delete',['class'=>'btn btn-danger'])}}
-            {{ Form::close() }}
-            
-            <a href="/posts/{{$posts->id}}/edit" class="btn btn-warning float-right" style="margin-right: 20px;">Edit</a>    
+            @if(!Auth::guest())
+               @if(Auth::user()->id == $posts->user_id)
+                    {{ Form::open(['action' => ['PostsController@destroy', $posts->id],'method'=>'POST','class'=>'float-right']) }}
+                        {{Form::hidden('_method','DELETE')}}
+                        {{ Form::submit('Delete',['class'=>'btn btn-danger'])}}
+                    {{ Form::close() }}
+                    
+                    <a href="/posts/{{$posts->id}}/edit" class="btn btn-warning float-right" style="margin-right: 20px;">Edit</a>    
+                @endif
+            @endif
         </div>
     </div>
 @endsection
