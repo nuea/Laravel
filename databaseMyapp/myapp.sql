a1f3fab5ed1253b36da9b5c6c4525a69d2fa2b6b
Nuea <nueha.pimkanok@gmail.com>
2018-04-24 11:23:02 +0700

    updata database

--- databaseMyapp/myapp.sql
+++ databaseMyapp/myapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2018 at 08:58 AM
+-- Generation Time: Apr 24, 2018 at 06:21 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -42,7 +42,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2018_04_10_082548_create_posts_table', 2),
-(4, '2018_04_21_043914_add_user_id_to_posts', 3);
+(4, '2018_04_21_043914_add_user_id_to_posts', 3),
+(5, '2018_04_24_021717_add_cover_image_to_posts', 4),
+(6, '2018_04_24_022755_add_cover_image_to_posts', 5);
 
 -- --------------------------------------------------------
 
@@ -68,19 +70,16 @@ CREATE TABLE `posts` (
   `body` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `cover_image` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `title`, `body`, `created_at`, `updated_at`, `user_id`) VALUES
-(1, 'Post One', 'This is the post one.', '2018-04-10 02:15:02', '2018-04-10 02:16:44', 1),
-(2, 'Post Two', 'This is the post two.', '2018-04-18 23:33:57', '2018-04-18 23:33:57', 1),
-(3, 'Post Three', 'This is the post three.', '2018-04-19 21:06:55', '2018-04-19 21:06:55', 1),
-(4, 'Post Four', 'This is the post four.', '2018-04-19 21:07:29', '2018-04-19 21:07:29', 1),
-(5, 'Post Five', 'This is the post five.', '2018-04-20 23:32:12', '2018-04-20 23:32:12', 1);
+INSERT INTO `posts` (`id`, `title`, `body`, `created_at`, `updated_at`, `user_id`, `cover_image`) VALUES
+(7, 'Post One', 'This is post one test image.', '2018-04-23 21:05:49', '2018-04-23 21:05:49', 2, 'noimage.jpg');
 
 -- --------------------------------------------------------
 
@@ -103,7 +102,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Miki', 'Miki@gmail.com', '$2y$10$tioZNm/MopqGwc4qei8wbeB3T/5VekCN6fnIBbFhVL7cgWin9mXge', 'mhEEcOGfdRPv8sOGdUPhCZpMRnOv1uC0DoPWP1aQH3B4UMBfoiVU2DqY54Fk', '2018-04-20 20:02:48', '2018-04-20 20:02:48');
+(1, 'Miki', 'Miki@gmail.com', '$2y$10$tioZNm/MopqGwc4qei8wbeB3T/5VekCN6fnIBbFhVL7cgWin9mXge', 'OYhbkTmW24J2yjUmK18W1SY3aHwRf0WLgncB08n5eWcL2K5KQaEY34lO6JZ9', '2018-04-20 20:02:48', '2018-04-20 20:02:48'),
+(2, 'Nedz', 'nedz@gmail.com', '$2y$10$.q9qcLNJnqTJTW3DD2VMZefmPCSJSyxr93vSHHT8vtocm1ZCYYqiC', 'pMhOGlFAyuPgIsrnZRyeMN2pvSzNl3WZLfSRDiqrGtXHhS7Nhlm3JSbPtUpj', '2018-04-21 00:29:38', '2018-04-21 00:29:38');
 
 --
 -- Indexes for dumped tables
@@ -142,19 +142,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
